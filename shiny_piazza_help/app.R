5d1e3c338a8434bae965165830e81d295567455c
hahe211 <hess@college.harvard.edu>
2018-11-21 13:11:36 -0500

    making sure commit goes thru

---
+++ shiny_piazza_help/app.R
@@ -0,0 +1,61 @@
+library(tidyverse)
+library(dplyr)
+library(shiny)
+library(fs)
+library(reshape2)
+
+
+table <- read_rds("all_congress.rds") %>% 
+  select(rep_win, rep_adv, accuracy, district) %>% 
+  distinct(district, rep_win, rep_adv, accuracy) %>% 
+  select(-district)
+
+
+
+
+# Define UI for application that draws a scatterplot
+ui <- fluidPage(
+  
+  # Application title
+  titlePanel("Sample Demographic and Poll Accuracy"),
+  
+  # Sidebar with a select input
+  sidebarLayout(
+    sidebarPanel(
+      selectInput(inputId = "category",
+                  label = "select X axis",
+                  choices =c(`Polled Republican Advantage` = "rep_adv",
+                             `Actual Republican Advantage` = "rep_win"))),
+    
+    
+    # Show a plot
+    mainPanel(
+      plotOutput("scatterPlot")
+    )
+  )
+)
+
+server <- function(input, output) {
+  
+  output$scatterPlot <- renderPlot({
+    
+    table <- table %>%
+      select(input$category, accuracy)
+    
+    table %>%
+      ggplot(aes(x = input$category, y = accuracy)) +
+      # create scatterplot
+      geom_point() +
+      # add title, subtitle
+      labs(title = case_when(input$category == "rep_adv" ~ "Rep_adv",
+                             input$category == "rep_win" ~ "Rep_win"),
+           x = case_when(input$category == "rep_adv" ~ "Rep_adv",
+                         input$category == "rep_win" ~ "rep_win"),
+           
+           y = "Poll Accuracy")
+    
+  })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
