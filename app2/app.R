edc67924732cd892795bd9c46eed4e4299d71e65
hahe211 <hess@college.harvard.edu>
2018-11-21 14:46:30 -0500

    things are finally working!

--- app2/app.R
+++ app2/app.R
@@ -7,8 +7,9 @@ library(reshape2)
 # 
 # table <- read_rds("all_congress.rds") %>% 
 #   select(rep_win, rep_adv, accuracy, district) %>% 
-#   distinct(district, state, rep_win, rep_adv, accuracy) %>% 
+#   distinct(district, state, rep_win, rep_adv, accuracy, race_eth) %>% 
 #   select(-district)
+
 
 table <- read_rds("all_congress.rds") %>% 
   mutate(race_eth = fct_collapse(race_eth,
@@ -18,7 +19,10 @@ table <- read_rds("all_congress.rds") %>%
                                "Other" = "Other",
                              "Hispanic" = "Hispanic"
                             
-  )) 
+  )) %>% 
+  distinct(district, rep_win, rep_adv, accuracy, race_eth) 
+  
+#MY ISSUE: - the graph doesn't responsd when I select different race/eth
 
 
 ui <- fluidPage(
@@ -45,13 +49,13 @@ ui <- fluidPage(
 server <- function(input, output) {
   output$plot <- renderPlot({
     # Subset the table dataset by the chosen race_eths
-    table <- subset(table,
-                   race_eth %in% input$race_eth)
+    table <- table %>% filter(race_eth == input$race_eth)
+    # table <- subset(table,
+    #                race_eth %in% input$race_eth)
     
     
     p <- ggplot(table, aes(rep_adv, accuracy)) +
       geom_point(size = input$size, col = input$colour) +
-      #scale_x_log10() +
       ggtitle(input$title)
     
      if (input$fit) {
